d820f6197149979ef10a96f4876a1bf3a841e86c
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-03-30 16:25:37 -0400

    Create 10071.cpp

---
+++ 10071.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int v,t;
+    while (cin >> v >> t) cout << t*v*2 << endl; 
+}
