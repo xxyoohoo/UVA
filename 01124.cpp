f2558b65aef03d5851ef685c5768085366484c6d
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-05-26 19:44:28 -0400

    Create 01124.cpp

---
+++ 01124.cpp
@@ -0,0 +1,10 @@
+//https://vjudge.net/solution/59836153/pTriyF0SYwp8bUYMrlqs
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    while(getline(cin,s)) cout << s << endl;
+}
