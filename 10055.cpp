aa82d4bf0b5950e21d692aab14bc9e320c68d516
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-05-26 19:56:01 -0400

    Create 10055.cpp

---
+++ 10055.cpp
@@ -0,0 +1,8 @@
+//https://vjudge.net/solution/51782179/ZSfWEFDQU0NiAfRDSIlb
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b;
+    while (cin >> a >> b) cout << abs(a-b) << endl;
+}
